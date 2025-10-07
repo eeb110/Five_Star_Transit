71c79a654aa19cb81d3c01e2777ffc13a75edddd
eeb110 <eeb110@pitt.edu>
2025-10-06 20:00:19 -0400

    Created repo

--- App.tsx
+++ App.tsx
@@ -1,45 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import {View, Text, Image, ScrollView, TextInput, StyleSheet} from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+    <ScrollView style={styles.screenContainer}>
+      <Text style={styles.headerText}>Welcome to BusTracker</Text>
+      <View>
+        <Text>Search bus routes</Text>
+        <Image
+          source={require('./assets/bus.png')}
+          style={{width: 370, height: 200}}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          margin: 20,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="Search"
       />
-    </View>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  screenContainer: {
+    padding: 20, // adds padding around the entire BusStop screen
+    paddingTop: 100,
+    backgroundColor: '#f8f9fa', // optional for contrast
   },
+  busContainer: {
+    marginBottom: 10, // spacing between buses
+    padding: 10, // padding inside each Bus component
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 4,
+    elevation: 2, // Android shadow
+  },
+  headerText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      },
 });
 
 export default App;
